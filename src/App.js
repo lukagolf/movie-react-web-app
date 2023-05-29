709ae118c8f0121a58ac27c942549aa810c2a9dc
lukagolf <78548325+lukagolf@users.noreply.github.com>
2023-05-28 21:37:02 -0400

    Resolve conflict in App.js

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import { BrowserRouter } from "react-router-dom";
 import { Routes, Route, Navigate } from "react-router";
 import Profile from "./pages/profile";
 import Search from "./pages/search";
-import Home from "./pages/home";
+import Home from "./pages/home/index";
 import Details from "./pages/details";
 import Login from "./pages/login";
 
 function App() {
   return (
     <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Navigate to="/home" />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/profile/*" element={<Profile />} />
-          <Route path="/details" element={<Details />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/profile/*" element={<Profile />} />
+        <Route path="/details" element={<Details />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </BrowserRouter>
   );
 }
