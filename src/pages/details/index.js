881ecd65df9658dbb36b997aca32c68df4b6fcdd
Julie Rakas <julierakas@Julies-MacBook-Pro.local>
2023-05-30 12:13:03 -0400

    Started details page

--- src/pages/details/index.js
+++ src/pages/details/index.js
@@ -1,7 +1,14 @@
+import React from "react";
+import MyNav from "../../common-components/nav";
+import MovieListItem from "./movie-info";
+
 function Details() {
     return (
-    <>
-    </>
+    <div>
+        <MyNav />
+        <MovieListItem/>
+
+    </div>
     );
 }
 
