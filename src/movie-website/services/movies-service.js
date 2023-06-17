b502e30a16ab83a76059ca85d3ae69744baf8ddb
Julie <rakas.j@northeastern.edu>
2023-06-17 17:40:03 -0400

    Updated TOP_MOVIES_API url

--- src/movie-website/services/movies-service.js
+++ src/movie-website/services/movies-service.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const MOVIES_API = 'https://api.themoviedb.org/3/search/movie';
+const TOP_MOVIES_API = 'https://api.themoviedb.org/3/movie/popular';
 
 export const findMovies = async (searchParams) => {
   const { title, actor, director, year, genre } = searchParams;
@@ -18,7 +19,7 @@ export const findMovies = async (searchParams) => {
 }
 
 export const findTopMovies = async () => {
-    const response = await axios.get(MOVIES_API, {
+    const response = await axios.get(TOP_MOVIES_API, {
         params: {
             api_key: 'ffdfb660a1488ae7f304368f73e0e7ec',
         },
