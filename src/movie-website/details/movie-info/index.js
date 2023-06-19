099a08a3057f86fb21caf299621aa43078eec287
lukagolf <jovanovic.l@northeastern.edu>
2023-06-19 15:54:52 -0400

    Check if there's a currentUser before calling dispatch

--- src/movie-website/details/movie-info/index.js
+++ src/movie-website/details/movie-info/index.js
@@ -9,79 +9,80 @@ import { useDispatch, useSelector } from "react-redux";
 import { findSavedMovie } from "../../services/saved-movies-service";
 
 const MovieListItem = () => {
-    const {currentUser} = useSelector(state => state.user);
-    const {savedMovies} = useSelector(state => state.savedMovies);
-    const { id } = useParams();
-    const [movie, setMovie] = useState(null);
+  const { currentUser } = useSelector(state => state.user);
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
 
-    useEffect(() => {
-        const fetchMovie = async () => {
-            const response = await axios.get(
-                `https://api.themoviedb.org/3/movie/${id}?api_key=ffdfb660a1488ae7f304368f73e0e7ec`
-            );
-            setMovie(response.data);
-        };
-        fetchMovie();
-    }, [id]);
+  useEffect(() => {
+    const fetchMovie = async () => {
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/movie/${id}?api_key=ffdfb660a1488ae7f304368f73e0e7ec`
+      );
+      setMovie(response.data);
+    };
+    fetchMovie();
+  }, [id]);
 
-    const dispatch = useDispatch();
-    useEffect(() => {
-      console.log("CUR USER", currentUser);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    console.log("CUR USER", currentUser);
+    if (currentUser) {
       dispatch(findAllSavedMoviesThunk(currentUser._id));
-    }, [currentUser]);
-
-     const handleSaveBtn = async() => {
-        try {
-          const userAndMovie = {userId: currentUser._id, ...movie};
-          await dispatch(createSavedMovieThunk(userAndMovie)).unwrap();
-          alert("Saving movie " + userAndMovie.title);
-        } catch (e) {
-          alert("Movie is already saved")
-        }
-     };
-
-    // Check if movie exists
-    if (!movie) {
-        return <div>Loading...</div>;
     }
+  }, [currentUser]);
 
-    return (
-      <div>
-        <div className="wd-video-details-background row">
-          <div className="d-none d-lg-flex row">
-            {currentUser && currentUser.role === "VIEWER" && (
-              <div className="wd-left-col col-1">
-                <SavedBtn fn={handleSaveBtn} />
-              </div>
-            )}
-            <div className="wd-details-col col-5">
-              <div className="wd-title-text">{movie.original_title}</div>
-              <br />
-              <div>
-                <b>Rating:</b> {movie.vote_average}
-              </div>
-              <br />
-              <div>
-                <b>Release date:</b> {movie.release_date}
-              </div>
-              <br />
-              <div>
-                <b>Summary:</b> {movie.overview}
-              </div>
+  const handleSaveBtn = async () => {
+    try {
+      const userAndMovie = { userId: currentUser._id, ...movie };
+      await dispatch(createSavedMovieThunk(userAndMovie)).unwrap();
+      alert("Saving movie " + userAndMovie.title);
+    } catch (e) {
+      alert("Movie is already saved")
+    }
+  };
+
+  // Check if movie exists
+  if (!movie) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <div className="wd-video-details-background row">
+        <div className="d-none d-lg-flex row">
+          {currentUser && currentUser.role === "VIEWER" && (
+            <div className="wd-left-col col-1">
+              <SavedBtn fn={handleSaveBtn} />
             </div>
-            <div className="wd-photo-col col-5">
-              <img
-                src={
-                  "https://image.tmdb.org/t/p/w440_and_h660_face/" +
-                  movie.poster_path
-                }
-                alt="Movie Poster"
-              />
+          )}
+          <div className="wd-details-col col-5">
+            <div className="wd-title-text">{movie.original_title}</div>
+            <br />
+            <div>
+              <b>Rating:</b> {movie.vote_average}
             </div>
-            <div className="wd-right-col col-1"></div>
+            <br />
+            <div>
+              <b>Release date:</b> {movie.release_date}
+            </div>
+            <br />
+            <div>
+              <b>Summary:</b> {movie.overview}
+            </div>
           </div>
+          <div className="wd-photo-col col-5">
+            <img
+              src={
+                "https://image.tmdb.org/t/p/w440_and_h660_face/" +
+                movie.poster_path
+              }
+              alt="Movie Poster"
+            />
+          </div>
+          <div className="wd-right-col col-1"></div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 export default MovieListItem;
--- src/movie-website/profile/profile-lists/movie-bucket-list/moviesBucketList.js
+++ src/movie-website/profile/profile-lists/movie-bucket-list/moviesBucketList.js
@@ -6,25 +6,27 @@ import { useSelector, useDispatch } from "react-redux";
 import { findAllSavedMoviesThunk } from "../../../services/saved-movies-thunks";
 
 function MoviesBucketList() {
-    const { currentUser } = useSelector((state) => state.user);
-    const { savedMovies } = useSelector((state) => state.savedMovies);
-    const dispatch = useDispatch();
-    useEffect(() => {
+  const { currentUser } = useSelector((state) => state.user);
+  const { savedMovies } = useSelector((state) => state.savedMovies);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (currentUser) {
       dispatch(findAllSavedMoviesThunk(currentUser._id));
-    }, []);
-    return (
-      <div>
-        <ul className="wd-profile-list list-group">
-          <h3>Saved Movies List</h3>
-          <br />
-          {savedMovies.map((movie) => (
-            <li>
-              <MovieBucketListItem movieInfo={movie}/>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
+    }
+  }, []);
+  return (
+    <div>
+      <ul className="wd-profile-list list-group">
+        <h3>Saved Movies List</h3>
+        <br />
+        {savedMovies.map((movie) => (
+          <li>
+            <MovieBucketListItem movieInfo={movie} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 
 }
 export default MoviesBucketList;
