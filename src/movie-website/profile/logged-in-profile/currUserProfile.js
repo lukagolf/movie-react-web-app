39e5d47e8f0f44cc803daeb4c908c433124d4e23
lukagolf <jovanovic.l@northeastern.edu>
2023-06-22 17:05:23 -0400

    Propagate multirole behaviour

--- src/movie-website/profile/logged-in-profile/currUserProfile.js
+++ src/movie-website/profile/logged-in-profile/currUserProfile.js
@@ -1,33 +1,31 @@
 import MyNav from "../../../nav-components/nav";
-import ProfileInfo from "../profileInfo";
-import ProfileLists from "../profile-lists/profileLists";
 import { useSelector } from "react-redux";
 import CurrentProfileInfo from "./profileInfo";
 import CurrentUserProfileLists from "./profileLists";
 
 function CurrentUserProfile() {
-    const { currentUser } = useSelector((state) => state.user);
-    console.log("CURRENT: ", currentUser);
-    return (
-        <>
-          {currentUser ? (
-            <MyNav
-              options={{
-                homePage: false,
-                search: true,
-                signIn: false,
-                profile: true,
-                signOut: true,
-              }}
-            />
-          ) : (
-            <MyNav />
-          )}
-          <CurrentProfileInfo/>
-          <CurrentUserProfileLists/>
-           </>
+  const { currentUser } = useSelector((state) => state.user);
 
-    )
+  return (
+    <>
+      {currentUser ? (
+        <MyNav
+          options={{
+            homePage: false,
+            search: true,
+            signIn: false,
+            profile: true,
+            signOut: true,
+          }}
+        />
+      ) : (
+        <MyNav />
+      )}
+      <CurrentProfileInfo />
+      {currentUser && <CurrentUserProfileLists key={currentUser.roles ? currentUser.roles.join() : ''} />}
+    </>
+
+  )
 }
 
 export default CurrentUserProfile;
